f6dc7f0236376f310759742d0704d00c914b3b8f
alexandrosantana <42390248+alexandrosantana@users.noreply.github.com>
2020-10-28 08:31:38 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/alexandro/git/HelloGit.java
@@ -0,0 +1,15 @@
+package br.com.alexandro.git;
+
+/**
+ * 
+ * @author Alex
+ * Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git");
+	}
+
+}
